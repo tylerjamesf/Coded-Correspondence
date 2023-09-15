628ee23c9ea68d12ba2cd71b62ad0ad9f0027698
tylerjamesf <5036869+tylerjamesf@users.noreply.github.com>
2023-09-15 18:00:30 -0400

    added encode for Caeser cipher

--- caeser_encode.py
+++ caeser_encode.py
@@ -1,14 +1,14 @@
-#file: caeser_decode.py
+#file: caeser_encode.py
 #author: tylerjamesf
 #date: September 15, 2023
 
 # ask the user for the message filename
-message = input("Enter the message code you wish to decipher: ")
+message = input("Enter the message you want to encode: ")
 
 # ask the user for the key offset for cipher
-offset = input("Enter the code offset key: ")
+offset = int(input("Enter the offset number key to use: "))
 
-#function to decode
+#function to encode
 def caeser_cipher(message, offset):
   
   alphabet_letters = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
@@ -17,7 +17,7 @@ def caeser_cipher(message, offset):
    if letter in "!?.,' ":
       output += letter
    else:
-     output += alphabet_letters[alphabet_letters.find(letter) + offset]
+     output += alphabet_letters[alphabet_letters.find(letter) - offset]
   return output
 
 print(caeser_cipher(message, offset))
