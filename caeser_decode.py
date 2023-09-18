6a557963c598d8fc238bbf7a045a7afaa1d8103c
tylerjamesf <5036869+tylerjamesf@users.noreply.github.com>
2023-09-18 17:35:50 -0400

    added import time

--- caeser_decode.py
+++ caeser_decode.py
@@ -2,14 +2,18 @@
 #author: tylerjamesf
 #date: September 15, 2023
 
+import time
+
 # ask the user for the message filename
 message = input("Enter the message code you wish to decipher: ")
 
 # ask the user for the key offset for cipher
 offset = input("Enter the code offset key: ")
-
+elapsed_time = 0
 #function to decode
 def caeser_cipher(message, offset):
+
+  start_time = time.time()
   
   alphabet_letters = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
   output = ''
@@ -18,6 +22,9 @@ def caeser_cipher(message, offset):
       output += letter
    else:
      output += alphabet_letters[alphabet_letters.find(letter) + offset]
-  return output
+  end_time = time.time()
+  elapsed_time = end_time - start_time
+  return output, elapsed_time
 
+print("Elapsed time: ", elapsed_time)
 print(caeser_cipher(message, offset))
